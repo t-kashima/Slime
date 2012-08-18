2b7cff4d2fc084a11358103ed903a1b37087912e
ctyo <ctyo@live.jp>
2012-08-19 06:18:34 +0900

    change load config

--- lib/Model.pm
+++ lib/Model.pm
@@ -3,7 +3,11 @@ use strict;
 use warnings;
 use MongoDB;
 use CGI::Carp qw(croak);
+use YAML::XS;
 use Data::Dumper;
+
+my $file_config = 'config.yaml';
+my $config = YAML::XS::LoadFile($file_config);
 
 # 0.00027778 => 31m
 my $ANGLE       = 0.00027778;
@@ -11,12 +15,11 @@ my $ANGLE       = 0.00027778;
 sub new {
     my ($class, %args) = @_;
     my $self = bless {
-        host     => $args{host},
-        username => $args{username},
-        password => $args{password},
-        db_name  => $args{db_name},
-        port     => $args{port}
-
+        host => $config->{DB_HOST},
+        port => $config->{DB_PORT},
+        username => $config->{DB_USERNAME},
+        password => $config->{DB_PASSWORD},
+        db_name => $config->{DB_NAME}
     }, $class;
     $self->{database} = $self->setup_database;
     return $self;
